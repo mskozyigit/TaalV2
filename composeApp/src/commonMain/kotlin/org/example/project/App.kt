aadb94f5343712387c804fce41575b31a2d49cfb
Your Name <you@example.com>
2026-04-30 23:15:49 +0200

    Fix: Correct icon import statements

--- composeApp/src/commonMain/kotlin/org/example/project/App.kt
+++ composeApp/src/commonMain/kotlin/org/example/project/App.kt
@@ -1,7 +1,6 @@
 package org.example.project
 
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
